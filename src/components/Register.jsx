d68dcf689487e38604c876f6119404f4e4b62271
Michael <michaelgbenle@gmail.com>
2022-05-28 21:06:32 +0100

    testing

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -171,7 +171,7 @@ const handleSubmit =(e)=> {
                                 <nav aria-label="breadcrumb">
                                     <ol class="breadcrumb">
                                       <li class="breadcrumb-item"><Link to="/">Home</Link></li>
-                                      <li class="breadcrumb-item active" aria-current="page">Register</li>
+                                      <li class="breadcrumb-item active" aria-current="page">Buyer Sign up</li>
                                     </ol>
                                   </nav>
                             </div>
--- src/pages/SellerLogin.jsx
+++ src/pages/SellerLogin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from "../components/Navbar"
-import SellerLoginComponent from "../components/Register"
+import SellerLoginComponent from "../components/SellerLogin"
 import Footer from "../components/Footer"
 
 
