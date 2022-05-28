4e6629bf3b5b64dc06510a60975caf2f34da0485
Michael <michaelgbenle@gmail.com>
2022-05-28 21:58:48 +0100

    testing

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -112,9 +112,9 @@ const [login, setLogin] = useState(
                             <div class="breadcrumb__wrapper">
                                 <nav aria-label="breadcrumb">
                                     <ol class="breadcrumb">
-                                      <li class="breadcrumb-item"><Link to="/">Home</Link></li>
-                                      <li class="breadcrumb-item active" aria-current="page"><Link to="/buyer/login">Buyer Sign in</Link></li>
-                                      
+                                      <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                      <li class="breadcrumb-item active" aria-current="page"><Link to="/login">Lnmnjnmjnogin</Link></li>
+                                      <Link to="/login">sdfgsdfgdg</Link>
                                     </ol>
                                   </nav>
                             </div>
@@ -127,7 +127,7 @@ const [login, setLogin] = useState(
                 <div class="row">
                     <div class="col-lg-8 offset-lg-2">
                             <div class="basic-login">
-                            <h3 class="text-center mb-60">Buyer Login</h3>
+                            <h3 class="text-center mb-60">Login From Here</h3>
                             <form method="POST" onSubmit={onSubmit}>
                                 <label for="name">Email Address <span>**</span></label>
                                 <input 
@@ -172,5 +172,3 @@ const [login, setLogin] = useState(
         </main>
     )
 }
-
-
