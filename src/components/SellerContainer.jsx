79be019a147df9cfbe7017a970c23d2e3fc35db8
Michael <michaelgbenle@gmail.com>
2022-06-01 15:50:23 +0100

    seller order

--- src/components/SellerContainer.jsx
+++ src/components/SellerContainer.jsx
@@ -214,66 +214,12 @@ export default function SellerContainer() {
   </div>
 </div>
 
+
             {/* End .row */}    
             <div className="row">
 
            <div className="tables">
-  <div className="dashboard-table">
-    <div className="heading">
-      <h2>Order Overview</h2>
-      <a href="#" className="btn">View All</a>
-    </div>
-    <table className="table-head">
-      <thead>
-        <tr><td>Name</td>
-          <td>Product</td>
-          <td>Price</td>
-          <td>Action</td>
-        </tr></thead>
-      <tbody>
-        <tr>
-          <td>Oreva Ansel</td>
-          <td>Television</td>
-          <td>$4,000</td>
-          <td>
-            <i className="far fa-eye" />
-            <i className="far fa-edit" />
-            <i className="far fa-trash-alt" />
-          </td>
-        </tr>
-        <tr>
-          <td>Oluchi Tiyan</td>
-          <td>Drapes</td>
-          <td>$2,000</td>
-          <td>
-            <i className="far fa-eye" />
-            <i className="far fa-edit" />
-            <i className="far fa-trash-alt" />
-          </td>
-        </tr>
-        <tr>
-          <td>Chuks Okoro</td>
-          <td>Toothpick</td>
-          <td>$1</td>
-          <td>
-            <i className="far fa-eye" />
-            <i className="far fa-edit" />
-            <i className="far fa-trash-alt" />
-          </td>
-        </tr>
-        <tr>
-          <td>Victor Ihemadu</td>
-          <td>Main.go</td>
-          <td>$20</td>
-          <td>
-            <i className="far fa-eye" />
-            <i className="far fa-edit" />
-            <i className="far fa-trash-alt" />
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  
 </div>
             </div> 
           </div>
