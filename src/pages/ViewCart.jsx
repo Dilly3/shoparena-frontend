558b59fea6404f79f40cb1d1adfbda2dfeb7d1cf
Kjasuquo <joseph.asuquo@decagon.dev>
2022-06-01 12:56:51 +0100

    view testing

---
+++ src/pages/ViewCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from "../components/Footer"
+import ViewCartComponent from "../components/ViewCart"
+
+export default function ViewCart() {
+    return(
+        <>
+        <ViewCartComponent/>
+        <Footer/>
+        </>
+    )
+}
