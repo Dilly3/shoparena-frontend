b9e254f1a25725016ef6ef0a0a10f54620d0b40f
coderUdu <uduvinci@gmail.com>
2022-06-03 01:42:26 +0100

    UpdatedsellerandIdanderror404

--- src/components/ProductId.jsx
+++ src/components/ProductId.jsx
@@ -1,15 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
-function ProductId({ id, Title, Category }) {
+const token = localStorage.getItem("token");
+
+function ProductId(props) {
+  const { id } = useParams();
+  const [product, updateProduct] = useState(null);
+
+  async function getProductDetails(id) {
+    // const { ID, price, image, title } = product;
+    try {
+      const response = await axios.get(
+        `https://oja-ecommerce.herokuapp.com/api/v1/product/${id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      updateProduct(response.data.Product);
+      console.log("Response: ", response);
+    } catch (error) {
+      console.log("Errors", error);
+    }
+  }
+
+  useEffect(() => {
+    getProductDetails(id);
+  }, []);
+
   return (
     <>
-      <div key="Id">
-        <Link to="/">Home</Link>
-        <h1>{id}</h1>
-        <h4>{Category}</h4>
-        <h4>{Title}</h4>
-      </div>
+      {product ? (
+        <div key="Id">
+          <Link to="/">Home</Link>
+          <h1>{product.ID}</h1>
+          <h4>"Category"</h4>
+          <h4>{product.title}</h4>
+        </div>
+      ) : (
+        <h1>loading...</h1>
+      )}
     </>
   );
 }
