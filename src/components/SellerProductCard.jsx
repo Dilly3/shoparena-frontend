b725348cc8fa3c56af74063208c19f7054d214f3
eniworoeva <eniworoeva@yahoo.com>
2022-06-09 12:57:00 +0100

    redesigned the seller shop

--- src/components/SellerProductCard.jsx
+++ src/components/SellerProductCard.jsx
@@ -29,27 +29,37 @@ useEffect(()=>{
     <div className='container-2'>
      { sellerShop.length > 0 ? sellerShop[0].product.map((product) => ( 
     
-    <div className="card-2">
-    <img src={product.images} alt />
-    <div className="card-body-2">
-      <div className="row-2">
-        <div className="card-title-2">
-          <h4>{ product.title }</h4>
-          <h3>N{product.price}</h3>
-        </div>
+  //   <div className="card-2">
+  //   <img src={product.images} alt />
+  //   <div className="card-body-2">
+  //     <div className="row-2">
+  //       <div className="card-title-2">
+  //         <h4>{ product.title }</h4>
+  //         <h3>N{product.price}</h3>
+  //       </div>
        
-      </div>
-      <hr />
-      <p>
-        {product.description}
-      </p>
-      <div className="btn-group-2">
-        <div className="btn-2">
-          <a className='buy-btn' href>Buy Now</a>
-        </div>
+  //     </div>
+  //     <hr />
+  //     <p>
+  //       {product.description}
+  //     </p>
+  //     <div className="btn-group-2">
+  //       <div className="btn-2">
+  //         <a className='buy-btn' href>Buy Now</a>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div> 
+
+  <div className="product-card">
+      <h1>{ product.title }</h1>
+      <p>{product.description}</p>
+      <img className="product-pic" src={product.images} alt="img" />
+      <div className="product-info">
+        <div className="product-price">N{product.price}</div>
+        <a href="#" className="product-button">Buy Now</a>
       </div>
     </div>
-  </div> 
   
        )) : <p>loading</p>} 
 </div>
