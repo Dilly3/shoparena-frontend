e168fc7e5c6d450854802c3954a6323892d8ef60
eniworoeva <eniworoeva@yahoo.com>
2022-06-15 12:47:04 +0100

    fixed the preloader bug on the main and sellers page

--- src/components/getAllSellers.jsx
+++ src/components/getAllSellers.jsx
@@ -34,76 +34,80 @@ const GetAllSellers = ({ id, Rating, Product, Order }) => {
 
   return (
     <>
-    {/* PRELOADER */}
-
-    <div id="loading">
-        <div id="loading-center">
+      {/* PRELOADER */}
+      {sellers === null ? (
+        <div id="loading">
+          <div id="loading-center">
             <div id="loading-center-absolute">
-                <div id="object"></div>
+              <div id="object"></div>
             </div>
+          </div>
         </div>
-    </div >
-      <div className="sellersNav">
-        <p>
-          <span className="sellerz">SELLER</span> INFORMATION
-        </p>
-      </div>
-      <div className="grid">
-        {sellers ? (
-          sellers.map((seller) => {
-            console.log(seller);
-            return (
-              <>
-                <div key={id}>
-                  <div className="sellerslayout">
-                    <section className="scontainer">
-                      {seller.image}
-                      <br />
-                      <h2>
-                        {seller.first_name} {seller.last_name}
-                      </h2>
-                      <br />
-                      <p className="contentbody">
-                        {seller.email}
-                        <br />
-                        <br />
-                        {seller.username}
-                        <br />
-                        {seller.address}
-                        <br />
-                        {seller.phone_number}
-                        <br />
-                        {seller.status}
-                        <br />
-                        {seller.token}
-                        <br />
-                        {seller.product}
-                        <br />
-                        {seller.orders}
-                      </p>
-                      <br />
-                      <br />
-                      {/*view and share seller shop link start*/}
-                      <div className="links">
-                        <Link to={`/seller/shop/${seller.ID}`}>
-                          <p className="store">
-                            View Store <AiFillShop />
+      ) : (
+        <div>
+          <div className="sellersNav">
+            <p>
+              <span className="sellerz">SELLER</span> INFORMATION
+            </p>
+          </div>
+          <div className="grid">
+            {sellers ? (
+              sellers.map((seller) => {
+                console.log(seller);
+                return (
+                  <>
+                    <div key={id}>
+                      <div className="sellerslayout">
+                        <section className="scontainer">
+                          {seller.image}
+                          <br />
+                          <h2>
+                            {seller.first_name} {seller.last_name}
+                          </h2>
+                          <br />
+                          <p className="contentbody">
+                            {seller.email}
+                            <br />
+                            <br />
+                            {seller.username}
+                            <br />
+                            {seller.address}
+                            <br />
+                            {seller.phone_number}
+                            <br />
+                            {seller.status}
+                            <br />
+                            {seller.token}
+                            <br />
+                            {seller.product}
+                            <br />
+                            {seller.orders}
                           </p>
-                        </Link>
-                        <div>
-                          <SocialShare social_id={seller.ID} />
-                        </div>
+                          <br />
+                          <br />
+                          {/*view and share seller shop link start*/}
+                          <div className="links">
+                            <Link to={`/seller/shop/${seller.ID}`}>
+                              <p className="store">
+                                View Store <AiFillShop />
+                              </p>
+                            </Link>
+                            <div>
+                              <SocialShare social_id={seller.ID} />
+                            </div>
+                          </div>
+                        </section>
                       </div>
-                    </section>
-                  </div>
-                </div>
-              </>
-            );
-          })
-        ) : (
-          <p className="fetch">Fetching Sellers...</p>
-        )}
-      </div>
+                    </div>
+                  </>
+                );
+              })
+            ) : (
+              <p className="fetch">Fetching Sellers...</p>
+            )}
+          </div>
+        </div>
+      )}
     </>
   );
 };
